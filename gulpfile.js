d12398281e479d7bb5f58b0a0ad84bb5ece0b423
YuriiHoliuk <bionicuss@gmail.com>
2017-01-11 23:00:53 +0200

    Added .gitignore, package.json, gulpfile.js, rename app to src

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
+    browserSync  = require('browser-sync'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglifyjs'),
+    cssnano      = require('gulp-cssnano'),
+    rename       = require('gulp-rename'),
+    del          = require('del'),
+    imagemin     = require('gulp-imagemin'),
+    pngquant     = require('imagemin-pngquant'),
+    cache        = require('gulp-cache'),
+    autoprefixer = require('gulp-autoprefixer'),
+    postcss      = require('gulp-postcss'),
+    mqpacker     = require("css-mqpacker"),
+    htmlmin      = require('gulp-htmlmin');
+
+gulp.task('sassConcat', function() {
+    return gulp.src(['src/scss/_import.scss', 'src/blocks/**/*.scss'])
+        .pipe(concat('main.scss'))
+        .pipe(gulp.dest('src/scss/'))
+});
+
+gulp.task('sass', ['sassConcat'], function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(autoprefixer(['last 5 versions', '> 1%'], { cascade: true }))
+        .pipe(postcss([mqpacker()]))
+        .pipe(gulp.dest('src/css'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'src'
+        },
+        notify: false
+    });
+});
+
+gulp.task('scripts', function() {
+    return gulp.src(['src/libs/jquery/dist/jquery.min.js'])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('src/js'));
+});
+
+gulp.task('css-min', ['sass'], function() {
+    return gulp.src(['src/css/libs.css', 'src/css/main.css'])
+        .pipe(cssnano())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', ['browser-sync', 'css-min', 'scripts'], function() {
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+    gulp.watch('src/blocks/**/*.scss', ['sass']);
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('img', function() {
+    return gulp.src('src/img/**/*.*')
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('buildHtml', function() {
+    return gulp.src('src/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('dist/'))
+})
+
+gulp.task('buildCss', function() {
+    return gulp.src(['src/css/*.min.css'])
+        .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('buildFonts', function() {
+    return gulp.src(['src/fonts/*.*'])
+        .pipe(gulp.dest('dist/fonts/'))
+})
+
+gulp.task('buildJs', function() {
+    return gulp.src(['src/js/*.js'])
+        .pipe(gulp.dest('dist/js/'))
+})
+
+gulp.task('buildImg', function() {
+    return gulp.src(['src/img/*.+(jpg|png|svg)'])
+        .pipe(gulp.dest('dist/img/'))
+})
+
+
+gulp.task('build', ['css-min', 'scripts', 'clean', 'img', 'buildHtml', 'buildCss', 'buildJs', 'buildFonts'])
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
+gulp.task('default', ['watch']);
